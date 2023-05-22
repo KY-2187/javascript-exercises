249700052718e1b42a04b91943439abe7abd51aa
KY-2187 <kyrahung1@gmail.com>
2023-05-22 16:10:52 +0800

    Add answer to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    let repeatedString = '';
 
+    if (number < 0) {
+        return 'ERROR';
+    } else {
+        counter = 0;
+        while (counter < number) {
+            repeatedString += word;
+            counter++;
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
