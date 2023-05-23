37b31940542abb86408fdbfbf2c91b4000b327dd
KY-2187 <kyrahung1@gmail.com>
2023-05-23 15:39:51 +0800

    Add answer to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!items.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
