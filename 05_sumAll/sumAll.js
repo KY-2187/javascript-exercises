a07a6a334327343f6e0e17782aae5fd9b4b00c0b
KY-2187 <kyrahung1@gmail.com>
2023-05-23 16:14:49 +0800

    Add answer to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    if (second > first) {
+        for (let i = first; i <= second; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = second; i <= first; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
