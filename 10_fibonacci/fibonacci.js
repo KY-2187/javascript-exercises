c0fdf24d707970b4ce2536f862d4c65656187daa
KY-2187 <kyrahung1@gmail.com>
2023-05-29 14:53:22 +0800

    Add answer to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return 'OOPS';
+    }
+    
+    let array = [];
+    for (let i = 0; i < number; i++) {
+        if (array.length == 0 || array.length == 1) {
+            array.push(1);
+        } else {
+            let sum = array[array.length - 2] + array[array.length - 1];
+            array.push(sum);
+        }
+    }
 
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
