26f9a7429436110de8067dc6ed099e896a537aaf
KY-2187 <kyrahung1@gmail.com>
2023-05-29 14:29:45 +0800

    Add answer to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    let string = s.toLowerCase().replace(/[^a-z]/g, "");
+    let p1 = 0;
+    let p2 = string.length -1;
 
+    while (p1 < p2) {
+        if (string[p1] !== string[p2]) {
+            return false;
+        }
+        p1++;
+        p2--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
